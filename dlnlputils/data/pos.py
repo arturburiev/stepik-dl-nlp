ba50ebe34f45ed319752ffc9e4fdd74db91466bd
Artur Buriev <arturburiev@yandex.ru>
2022-04-20 15:38:09 +0400

    Formatted code

--- dlnlputils/data/pos.py
+++ dlnlputils/data/pos.py
@@ -6,11 +6,15 @@ from .base import tokenize_corpus
 
 
 def pos_corpus_to_tensor(sentences, char2id, label2id, max_sent_len, max_token_len):
-    inputs = torch.zeros((len(sentences), max_sent_len, max_token_len + 2), dtype=torch.long)
+    inputs = torch.zeros(
+        (len(sentences), max_sent_len, max_token_len + 2), dtype=torch.long
+    )
     targets = torch.zeros((len(sentences), max_sent_len), dtype=torch.long)
 
     for sent_i, sent in enumerate(sentences):
         for token_i, token in enumerate(sent):
+            if token.form is None or token.upos is None:
+                continue
             targets[sent_i, token_i] = label2id.get(token.upos, 0)
             for char_i, char in enumerate(token.form):
                 inputs[sent_i, token_i, char_i + 1] = char2id.get(char, 0)
@@ -29,7 +33,10 @@ class POSTagger:
     def __call__(self, sentences):
         tokenized_corpus = tokenize_corpus(sentences, min_token_size=1)
 
-        inputs = torch.zeros((len(sentences), self.max_sent_len, self.max_token_len + 2), dtype=torch.long)
+        inputs = torch.zeros(
+            (len(sentences), self.max_sent_len, self.max_token_len + 2),
+            dtype=torch.long,
+        )
 
         for sent_i, sentence in enumerate(tokenized_corpus):
             for token_i, token in enumerate(sentence):
@@ -37,10 +44,14 @@ class POSTagger:
                     inputs[sent_i, token_i, char_i + 1] = self.char2id.get(char, 0)
 
         dataset = TensorDataset(inputs, torch.zeros(len(sentences)))
-        predicted_probs = predict_with_model(self.model, dataset)  # SentenceN x TagsN x MaxSentLen
+        predicted_probs = predict_with_model(
+            self.model, dataset
+        )  # SentenceN x TagsN x MaxSentLen
         predicted_classes = predicted_probs.argmax(1)
 
         result = []
         for sent_i, sent in enumerate(tokenized_corpus):
-            result.append([self.id2label[cls] for cls in predicted_classes[sent_i, :len(sent)]])
+            result.append(
+                [self.id2label[cls] for cls in predicted_classes[sent_i, : len(sent)]]
+            )
         return result
